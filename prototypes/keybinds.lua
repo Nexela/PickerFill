2d0be76e2892aa3f6819572a58a7dbcd47b51457
Nicholas Dunn <nexela@gmail.com>
2022-07-20 02:53:45 -0400

    More Formatting

--- prototypes/keybinds.lua
+++ prototypes/keybinds.lua
@@ -1,13 +1,13 @@
-local Data = require('__stdlib__/stdlib/data/data')
+local Data = require("__stdlib__/stdlib/data/data")
 
-Data{
-    type = 'custom-input',
-    name = 'picker-toggle-autofill-gui',
-    key_sequence = 'CONTROL + ALT + A',
+Data {
+    type = "custom-input",
+    name = "picker-toggle-autofill-gui",
+    key_sequence = "CONTROL + ALT + A",
 }
 
-Data{
-    type = 'custom-input',
-    name = 'picker-hotkey-fill',
-    key_sequence = 'F'
+Data {
+    type = "custom-input",
+    name = "picker-hotkey-fill",
+    key_sequence = "F"
 }
